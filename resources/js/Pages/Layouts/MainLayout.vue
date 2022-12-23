696b947da328aabd84a671ff619382748ef97fc6
gitocrama <spocram@gmail.com>
2022-12-23 18:11:49 +0100

    restore HandleInertiaRequests middleware

--- resources/js/Pages/Layouts/MainLayout.vue
+++ resources/js/Pages/Layouts/MainLayout.vue
@@ -6,6 +6,10 @@
                     <Link :href="route('listing.index')">Listings</Link>
                 </div>
                 <div class="text-xl text-indigo-600 dark:text-indigo-300 font-bold text-center">
+                    <span v-if="user" class="text-sm text-white">
+                        {{ user.name }}
+                    <br>
+                    </span>
                     <Link :href="route('listing.index')">ViltApp</Link>
                 </div>
                 <div>
@@ -16,10 +20,10 @@
     </header>
 
     <main class="container mx-auto p-4">
-<!--        <div v-if="flashSuccess" class="mb-4 border rounded-md shadow-sm border-green-200 dark:border-green-800 bg-green-50 dark:bg-green-900 p-2">-->
-<!--            {{ flashSuccess }}-->
-<!--        </div>-->
-        <slot>Default</slot>
+        <div v-if="flashSuccess" class="mb-4 border rounded-md shadow-sm border-green-200 dark:border-green-800 bg-green-50 dark:bg-green-900 p-2">
+            {{ flashSuccess }}
+        </div>
+        <slot />
     </main>
 </template>
 
@@ -30,5 +34,8 @@ import { Link, usePage } from '@inertiajs/inertia-vue3'
 const page = usePage()
 const flashSuccess = computed(
     () => page.props.value.flash.success
+)
+const user = computed(
+    () => page.props.value.user
 )
 </script>
